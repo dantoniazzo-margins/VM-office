df4379d16b20c9c23bd5a27ed7fbfc9d443995a1
dantoniazzo <85890036+dantoniazzo@users.noreply.github.com>
2024-11-03 01:16:06 +0100

    Feature: add right wheel and joint on axle

--- src/_entities/car/ui/AxleWithWheels.tsx
+++ src/_entities/car/ui/AxleWithWheels.tsx
@@ -1,44 +1,48 @@
-import { Box, useKeyboardControls } from "@react-three/drei";
+import { Box, useKeyboardControls } from '@react-three/drei';
 import {
+  CuboidCollider,
+  CylinderCollider,
   RapierRigidBody,
   RigidBody,
   useRevoluteJoint,
-} from "@react-three/rapier";
-import { useRef } from "react";
-import { Wheel } from "./Wheel";
-import { useFixedJoint } from "@react-three/rapier";
-import { useFrame } from "@react-three/fiber";
+} from '@react-three/rapier';
+import { useRef } from 'react';
+import { Wheel } from './Wheel';
+import { useFixedJoint } from '@react-three/rapier';
+import { useFrame } from '@react-three/fiber';
 
 interface WheelProps {
   worldPosition: [number, number, number];
 }
 export const AxleWithWheels = (props: WheelProps) => {
   const bodyRef = useRef<RapierRigidBody>(null);
+  const smallBoxPosition: [number, number, number] = [2.5, 0.5, 0];
   const smallBox = useRef<RapierRigidBody>(null);
+  const secondBoxPosition: [number, number, number] = [0.6, 0, 0];
   const secondBox = useRef<RapierRigidBody>(null);
   const rightWheelRef = useRef<RapierRigidBody>(null);
-  const joint = useRevoluteJoint(smallBox, bodyRef, [
+  const joint = useRevoluteJoint(bodyRef, smallBox, [
+    [2.5, 0.35, 0],
     [0, 0, 0],
-    [6, 0, 0],
-    [1, 0, 0],
+    [0, 1, 0],
   ]);
 
   const joint2 = useRevoluteJoint(smallBox, secondBox, [
+    secondBoxPosition,
     [0, 0, 0],
-    [0, 0, 2],
-    [0, 0, 1],
+    [1, 0, 0],
   ]);
 
   const [_, getKeys] = useKeyboardControls();
   useFrame(() => {
     if (joint.current && smallBox.current) {
       const keys = getKeys();
-      if (keys.forward) {
+      if (keys.left) {
         smallBox.current.wakeUp();
-        joint.current.configureMotorVelocity(50, 10);
-      } else if (keys.backward) {
+        joint.current.configureMotorVelocity(100, 10);
+      } else if (keys.right) {
         smallBox.current.wakeUp();
-        joint.current.configureMotorVelocity(-50, 10);
+        joint.current.configureMotorVelocity(-100, 10);
       } else joint.current.configureMotorVelocity(0, 10);
     }
 
@@ -46,39 +50,40 @@ export const AxleWithWheels = (props: WheelProps) => {
       const keys = getKeys();
       if (keys.forward) {
         secondBox.current.wakeUp();
-        joint2.current.configureMotorVelocity(50, 10);
+        joint2.current.configureMotorVelocity(-1000, 10);
       } else if (keys.backward) {
         secondBox.current.wakeUp();
-        joint2.current.configureMotorVelocity(-50, 10);
+        joint2.current.configureMotorVelocity(1000, 10);
       } else joint2.current.configureMotorVelocity(0, 10);
     }
   });
   return (
-    <>
-      <RigidBody colliders="cuboid" ref={bodyRef} type="dynamic">
+    <group>
+      <RigidBody
+        position={props.worldPosition}
+        colliders="cuboid"
+        ref={bodyRef}
+        type="dynamic"
+      >
         <Box args={[5, 0.5, 0.5]} castShadow receiveShadow name="axle">
-          <meshStandardMaterial color={"red"} />
+          <meshStandardMaterial color={'red'} />
         </Box>
       </RigidBody>
       <RigidBody
-        position={[6, 0, 0]}
+        position={smallBoxPosition}
         colliders="cuboid"
         ref={smallBox}
         type="dynamic"
       >
-        <Box args={[0.5, 0.5, 0.5]} castShadow receiveShadow name="axle">
-          <meshStandardMaterial color={"blue"} />
-        </Box>
+        <CuboidCollider args={[0.3, 0.1, 0.1]} />
       </RigidBody>
       <RigidBody
-        position={[6, 0, 4]}
+        position={secondBoxPosition}
         colliders="cuboid"
         ref={secondBox}
         type="dynamic"
       >
-        <Box args={[0.5, 0.5, 0.5]} castShadow receiveShadow name="axle">
-          <meshStandardMaterial color={"purple"} />
-        </Box>
+        <CylinderCollider rotation={[0, 0, Math.PI / 2]} args={[0.15, 0.7]} />
       </RigidBody>
       {/*  <Wheel
         worldPosition={[40, 0, 0]}
@@ -91,6 +96,6 @@ export const AxleWithWheels = (props: WheelProps) => {
         worldPosition={[-300, 0, 0]}
         bodyPosition="right"
       /> */}
-    </>
+    </group>
   );
 };
--- src/_pages/DrivingTest/DrivingTest.tsx
+++ src/_pages/DrivingTest/DrivingTest.tsx
@@ -1,11 +1,11 @@
-import { Canvas } from "@react-three/fiber";
-import { RigidBody, Physics } from "@react-three/rapier";
+import { Canvas } from '@react-three/fiber';
+import { RigidBody, Physics } from '@react-three/rapier';
 import {
   KeyboardControls,
   OrbitControls,
   PerspectiveCamera,
-} from "@react-three/drei";
-import { ThreeCar, Wheel, AxleWithWheels } from "_entities/car";
+} from '@react-three/drei';
+import { ThreeCar, Wheel, AxleWithWheels } from '_entities/car';
 
 function Scene() {
   return (
@@ -18,7 +18,7 @@ function Scene() {
       <Physics debug gravity={[0, -9.81, 0]}>
         <AxleWithWheels worldPosition={[0, 1, 0]} />
         <RigidBody type="fixed">
-          <mesh receiveShadow position={[0, -2, 0]}>
+          <mesh receiveShadow position={[0, -1, 0]}>
             <boxGeometry args={[200, 1, 200]} />
             <meshStandardMaterial color="green" />
           </mesh>
@@ -30,17 +30,17 @@ function Scene() {
 
 export default function App() {
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
+    <div style={{ height: '100vh', width: '100vw' }}>
       <KeyboardControls
         map={[
-          { name: "forward", keys: ["ArrowUp", "KeyW"] },
-          { name: "backward", keys: ["ArrowDown", "KeyS"] },
-          { name: "left", keys: ["ArrowLeft", "KeyA"] },
-          { name: "right", keys: ["ArrowRight", "KeyD"] },
-          { name: "flip", keys: ["KeyF"] },
-          { name: "shift", keys: ["Shift"] },
-          { name: "jump", keys: ["Space"] },
-          { name: "reset", keys: ["KeyR"] },
+          { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
+          { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
+          { name: 'left', keys: ['ArrowLeft', 'KeyA'] },
+          { name: 'right', keys: ['ArrowRight', 'KeyD'] },
+          { name: 'flip', keys: ['KeyF'] },
+          { name: 'shift', keys: ['Shift'] },
+          { name: 'jump', keys: ['Space'] },
+          { name: 'reset', keys: ['KeyR'] },
         ]}
       >
         <Scene />
