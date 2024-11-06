eb11f0887b9e18e719911ed653b309693cb249e2
dantoniazzo <85890036+dantoniazzo@users.noreply.github.com>
2024-11-07 00:13:33 +0100

    Fix: add hull collider for tv setup items

--- src/_entities/tv-table/ui/TvTable.tsx
+++ src/_entities/tv-table/ui/TvTable.tsx
@@ -5,7 +5,7 @@ export const TvTable = (props: JSX.IntrinsicElements['group']) => {
   const tvTable = useGLTF('/tv_table.glb');
 
   return (
-    <RigidBody colliders="trimesh" type="fixed" restitution={0} friction={0.7}>
+    <RigidBody colliders="hull" type="fixed" restitution={0} friction={0.7}>
       <primitive
         position-y={-1.35}
         {...props}
--- src/_entities/tv/ui/TV.tsx
+++ src/_entities/tv/ui/TV.tsx
@@ -3,7 +3,7 @@ import { RigidBody } from '@react-three/rapier';
 export const TV = (props: JSX.IntrinsicElements['group']) => {
   const tv = useGLTF('/tv.glb');
   return (
-    <RigidBody colliders="trimesh" type="fixed" restitution={0} friction={0.7}>
+    <RigidBody colliders="hull" type="fixed" restitution={0} friction={0.7}>
       <primitive {...props} object={tv.scene} castShadow />
     </RigidBody>
   );
