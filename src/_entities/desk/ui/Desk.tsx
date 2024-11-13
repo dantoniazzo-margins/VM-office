f482f072cd23ace2d463fb69273c4e1af2652f10
Dorian Antoniazzo <dorian.antoniazzo@bebabit.com>
2024-11-13 01:19:49 +0100

    Fix: desk leg connector height

--- src/_entities/desk/ui/Desk.tsx
+++ src/_entities/desk/ui/Desk.tsx
@@ -1,7 +1,7 @@
-import { RigidBody } from '@react-three/rapier';
-import { DESK_LEG_SIZE, DESK_SIZE } from '../lib/constants';
+import { RigidBody } from "@react-three/rapier";
+import { DESK_LEG_SIZE, DESK_SIZE } from "../lib/constants";
 
-export default function Desk(props: JSX.IntrinsicElements['group']) {
+export default function Desk(props: JSX.IntrinsicElements["group"]) {
   return (
     <RigidBody colliders="cuboid" type="dynamic" restitution={0} friction={0.7}>
       <group position-y={-0.4} {...props}>
@@ -33,7 +33,7 @@ export default function Desk(props: JSX.IntrinsicElements['group']) {
           />
           <meshPhongMaterial color="black" />
         </mesh>
-        <mesh position={[-0.95, -0.5 - DESK_SIZE.y + DESK_LEG_SIZE.z / 2, 0]}>
+        <mesh position={[-0.95, -0.65 - DESK_SIZE.y + DESK_LEG_SIZE.z / 2, 0]}>
           <boxGeometry
             args={[DESK_LEG_SIZE.x, DESK_LEG_SIZE.z, 1 - DESK_LEG_SIZE.z * 2]}
           />
@@ -63,7 +63,7 @@ export default function Desk(props: JSX.IntrinsicElements['group']) {
           />
           <meshPhongMaterial color="black" />
         </mesh>
-        <mesh position={[0.95, -0.5 - DESK_SIZE.y + DESK_LEG_SIZE.z / 2, 0]}>
+        <mesh position={[0.95, -0.65 - DESK_SIZE.y + DESK_LEG_SIZE.z / 2, 0]}>
           <boxGeometry
             args={[DESK_LEG_SIZE.x, DESK_LEG_SIZE.z, 1 - DESK_LEG_SIZE.z * 2]}
           />
