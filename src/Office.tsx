e55ed506258c2dd630fb115b3bd6657d39a96039
Dorian Antoniazzo <dorian.antoniazzo@bebabit.com>
2025-05-21 11:03:11 +0200

    Fix: merge confilct

--- src/Office.tsx
+++ src/Office.tsx
@@ -1,32 +1,53 @@
-import { OrbitControls } from '@react-three/drei';
-import { Perf } from 'r3f-perf';
-import { Physics } from '@react-three/rapier';
-import { Floor } from '_entities/floor';
-import { BoundingWalls } from '_widgets/BoundingWalls';
-import { DevSpace } from '_widgets/DevSpace';
-import { MiddleSpace } from '_widgets/MiddleSpace';
-import { DesignSpace } from '_widgets/DesignSpace';
-import { Reception } from '_widgets/Reception';
-import { Lounge } from '_widgets/Lounge';
-import { Player } from '_features/player';
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import { Physics } from "@react-three/rapier";
+import { Floor } from "_entities/floor";
+import { BoundingWalls } from "_widgets/BoundingWalls";
+import { DevSpace } from "_widgets/DevSpace";
+import { MiddleSpace } from "_widgets/MiddleSpace";
+import { DesignSpace } from "_widgets/DesignSpace";
+import { Reception } from "_widgets/Reception";
+import { Lounge } from "_widgets/Lounge";
+import { Player } from "_features/player";
 import {
   MAIN_FLOOR_LENGTH,
   MAIN_FLOOR_WIDTH,
   MAIN_FLOOR_HEIGHT,
-} from '_entities/floor';
-import { Vector3 } from 'three';
-import { Kitchen } from '_widgets/Kitchen';
-import { PMSpace } from '_widgets/PMSpace';
-import { GreenConf } from '_widgets/GreenConf';
-import { Ceiling } from '_widgets/Ceiling';
+} from "_entities/floor";
+import { Vector3 } from "three";
+import { Kitchen } from "_widgets/Kitchen";
+import { PMSpace } from "_widgets/PMSpace";
+import { GreenConf } from "_widgets/GreenConf";
+import { getProject } from "@theatre/core";
+import studio from "@theatre/studio";
+import extension from "@theatre/r3f/dist/extension";
+import { editable as e, SheetProvider } from "@theatre/r3f";
+import { PerspectiveCamera } from "@theatre/r3f";
+import { Ceiling } from "_widgets/Ceiling";
+
+studio.initialize();
+studio.extend(extension);
+// our Theatre.js project sheet, we'll use this later
+const demoSheet = getProject("Demo Project").sheet("Demo Sheet");
 
 export default function Office() {
   return (
-    <>
-      {process.env.NODE_ENV === 'development' && <Perf position="top-left" />}
+    <SheetProvider sheet={demoSheet}>
+      <PerspectiveCamera
+        theatreKey="Camera"
+        makeDefault
+        position={[5, 5, -5]}
+        fov={75}
+      />
+      {process.env.NODE_ENV === "development" && <Perf position="top-left" />}
 
       {/* <OrbitControls makeDefault /> */}
-      <directionalLight castShadow position={[7, 2, 0]} intensity={4.5} />
+      <e.directionalLight
+        theatreKey="light"
+        castShadow
+        position={[7, 2, 0]}
+        intensity={4.5}
+      />
       <ambientLight intensity={1.5} />
 
       <Physics gravity={[0, -9.81, 0]}>
@@ -47,6 +68,6 @@ export default function Office() {
         <GreenConf />
         <Player />
       </Physics>
-    </>
+    </SheetProvider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import './style.css';
-import ReactDOM from 'react-dom/client';
-import { Canvas } from '@react-three/fiber';
-import Office from './Office';
-import { KeyboardControls } from '@react-three/drei';
-import ThirdPersonTest from '_pages/ThirdPersonTest/ThirdPersonTest';
-import WalkingTest from '_pages/WalkingTest/WalkingTest';
-import DrivingGame from '_pages/DrivingTest/DrivingTest';
-import { RaceTrack } from '_pages/DrivingTest/Racetrack';
-import * as THREE from 'three';
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Office from "./Office";
+import { KeyboardControls } from "@react-three/drei";
+import ThirdPersonTest from "_pages/ThirdPersonTest/ThirdPersonTest";
+import WalkingTest from "_pages/WalkingTest/WalkingTest";
+import DrivingGame from "_pages/DrivingTest/DrivingTest";
+import { RaceTrack } from "_pages/DrivingTest/Racetrack";
+import * as THREE from "three";
 
-const rootContainer = document.querySelector('#root');
+const rootContainer = document.querySelector("#root");
 
 if (rootContainer) {
   const root = ReactDOM.createRoot(rootContainer);
@@ -17,25 +17,17 @@ if (rootContainer) {
   root.render(
     <KeyboardControls
       map={[
-        { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
-        { name: 'back', keys: ['ArrowDown', 'KeyS'] },
-        { name: 'left', keys: ['ArrowLeft', 'KeyA'] },
-        { name: 'right', keys: ['ArrowRight', 'KeyD'] },
-        { name: 'brake', keys: ['Space'] },
-        { name: 'jump', keys: ['Space'] },
-        { name: 'shift', keys: ['Shift'] },
-        { name: 'reset', keys: ['KeyR'] },
+        { name: "forward", keys: ["ArrowUp", "KeyW"] },
+        { name: "back", keys: ["ArrowDown", "KeyS"] },
+        { name: "left", keys: ["ArrowLeft", "KeyA"] },
+        { name: "right", keys: ["ArrowRight", "KeyD"] },
+        { name: "brake", keys: ["Space"] },
+        { name: "jump", keys: ["Space"] },
+        { name: "shift", keys: ["Shift"] },
+        { name: "reset", keys: ["KeyR"] },
       ]}
     >
-      <Canvas
-        shadows
-        /*   camera={{
-          fov: 45,
-          near: 0.5,
-          far: 200,
-          position: [-4.5, 7, 40],
-        }} */
-      >
+      <Canvas shadows>
         <Office />
       </Canvas>
     </KeyboardControls>
