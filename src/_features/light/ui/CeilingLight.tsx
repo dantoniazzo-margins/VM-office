db60b7ad7939ba1654781b8be35919c567bea362
Dorian Antoniazzo <dorian.antoniazzo@bebabit.com>
2025-05-21 12:13:47 +0200

    feat: refactor CeilingLight component and update Reception to use multiple instances

--- src/_features/light/ui/CeilingLight.tsx
+++ src/_features/light/ui/CeilingLight.tsx
@@ -1,16 +1,28 @@
+import { forwardRef, useMemo } from "react";
+import {
+  RapierRigidBody,
+  RigidBody,
+  RigidBodyProps,
+} from "@react-three/rapier";
+import { useGLTF } from "@react-three/drei";
+
 interface CeilingLightProps {
   "position-x"?: number;
   "position-z"?: number;
 }
 
-export const CeilingLight = (props: CeilingLightProps) => {
+export const CeilingLight = forwardRef((props: CeilingLightProps) => {
+  const ceilingLight = useGLTF("/ceiling_light.glb");
+  // Clone the scene to be able to use multiple instances.
+  const scene = useMemo(() => ceilingLight.scene.clone(), [ceilingLight]);
   return (
-    <pointLight
-      color={"white"}
-      intensity={5}
-      position-y={1}
+    <group
+      position-y={1.35}
       position-x={props["position-x"]}
       position-z={props["position-z"]}
-    />
+    >
+      <primitive scale={0.25} object={scene} castShadow />
+      <pointLight position-y={-0.1} color={"white"} intensity={5} />
+    </group>
   );
-};
+});
--- src/_widgets/Reception/ui/Reception.tsx
+++ src/_widgets/Reception/ui/Reception.tsx
@@ -1,11 +1,14 @@
 import { Wall } from "_entities/wall";
 import { ReceptionDesk } from "_entities/reception";
+import { CeilingLight } from "_features/light";
 
 export const Reception = () => {
   return (
     <group position-z={17.5}>
       <Wall length={2} rotate color="#2f2e2e" />
       <ReceptionDesk position-x={-1.5} />
+      <CeilingLight position-x={-3.7} position-z={0} />
+      <CeilingLight position-x={-3.7} position-z={5} />
     </group>
   );
 };
